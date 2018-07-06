6916b3673bc7e9afd27595d55dd0230a0b6a9e96
Matteo Fabbri <matteo@phascode.org>
2018-07-06 15:12:34 +0200

    fixed name confflict across assemblies

---
+++ ServerSideAnalytics.Extensions/AnalyticStoreExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Maddalena;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSideAnalytics.Extensions
+{
+    public static class AnalyticStoreExtensions
+    {
+        public static async Task<double> DailyAverage(this IAnalyticStore analyticStore,
+            DateTime from, DateTime to)
+        {
+            return (await analyticStore.CountAsync(from, to)) / (to - from).TotalDays;
+        }
+
+        public static async Task<IEnumerable<(DateTime Day, long Served)>> DailyServed
+            (this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.Timestamp.Date)
+                .Select(x => (x.Key, x.LongCount()));
+        }
+
+        public static async Task<IEnumerable<(int Hour, long Served)>> HourlyServed
+    (this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.Timestamp.Hour)
+                .Select(x => (x.Key, x.LongCount()));
+        }
+
+        public static async Task<IEnumerable<(string Url, long Served)>> UrlServed
+    (this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.Path)
+                .Select(x => (x.Key, x.LongCount()));
+        }
+
+        public static async Task<IEnumerable<(string Country, long Served)>> ServedByCountry
+(this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.CountryCode)
+                .Select(x => (Country.FromCode(x.Key).CommonName, x.LongCount()));
+        }
+    }
+}
--- ServerSideAnalytics/Extensions.cs
+++ ServerSideAnalytics/Extensions.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 
 namespace ServerSideAnalytics
 {
-    public static class Extensions
+    public static class ServerSideExtensions
     {
         public static string UserIdentity(this HttpContext context)
         {
