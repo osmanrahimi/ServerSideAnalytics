2796d54e59748d83f9e959d753b5ca919f2be1c1
Matteo Fabbri <matteo@phascode.org>
2018-07-06 14:41:26 +0200

    nuget

--- ServerSideAnalytics.Extensions/Extensions.cs
+++ ServerSideAnalytics.Extensions/Extensions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Maddalena;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ServerSideAnalytics.Extensions
@@ -12,10 +14,36 @@ namespace ServerSideAnalytics.Extensions
             return (await analyticStore.CountAsync(from, to)) / (to - from).TotalDays;
         }
 
-        public static async Task<IEnumerable<(DateTime Day, long Served)>> DailyEvera
+        public static async Task<IEnumerable<(DateTime Day, long Served)>> DailyServed
             (this IAnalyticStore analyticStore, DateTime from, DateTime to)
         {
-            return (await analyticStore.(from, to)) / (to - from).TotalDays;
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.Timestamp.Date)
+                .Select(x => (x.Key, x.LongCount()));
+        }
+
+        public static async Task<IEnumerable<(int Hour, long Served)>> HourlyServed
+    (this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.Timestamp.Hour)
+                .Select(x => (x.Key, x.LongCount()));
+        }
+
+        public static async Task<IEnumerable<(string Url, long Served)>> UrlServed
+    (this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.Path)
+                .Select(x => (x.Key, x.LongCount()));
+        }
+
+        public static async Task<IEnumerable<(string Country, long Served)>> ServedByCountry
+(this IAnalyticStore analyticStore, DateTime from, DateTime to)
+        {
+            return (await analyticStore.InTimeRange(from, to))
+                .GroupBy(x => x.CountryCode)
+                .Select(x => (Country.FromCode(x.Key).CommonName, x.LongCount()));
         }
     }
 }
